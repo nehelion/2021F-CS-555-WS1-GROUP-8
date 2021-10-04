867eec3ed3206a0a34b4e04db89736901ed2533f
Alexander Kulpin <alexandtoddy@gmail.com>
2021-10-04 15:07:40 -0400

    Project 3 Updates
    Updated code based on Project 3 requirements, output test included

--- main_project.java
+++ main_project.java
@@ -9,16 +9,25 @@ import java.util.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.Scanner; 
 import java.nio.charset.StandardCharsets;
 
 public class main_project
 {	
+	public static ArrayList<Individual> individuals = new ArrayList<Individual>();
+	public static ArrayList<Family> families = new ArrayList<Family>();
+
     public static void main(String[] args) throws Exception
 	{
 		int c = 0;
 		String valid = "N";
 		
-		Path path = Paths.get("Kulpin_Family.ged");
+		Scanner myObj = new Scanner(System.in);
+		System.out.print("Enter Path of GED file: ");
+		
+		String pathName = myObj.nextLine();
+		
+		Path path = Paths.get(pathName);
 		
 		String strOut = Files.readString(path, StandardCharsets.ISO_8859_1);
 		
@@ -27,6 +36,14 @@ public class main_project
 		for(int i = 0; i < lines.length; i++)
 		{
 			System.out.println("--> " + lines[i]);
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			for(int j = 0; j < lines[i].length(); j++)
 			{
@@ -42,6 +59,44 @@ public class main_project
 				{
 					valid = "Y";
 					System.out.println("<-- " + sections[0] + "|" + sections[2] + "|" + valid + "|" +  sections[1]);
+					
+					if(sections[2].equals("INDI"))
+					{
+						String[] sectionsName = lines[i + 1].split(" ", 3);
+						String[] sectionsGender = lines[i + 5].split(" ", 3);
+						String[] sectionsBirthday = lines[i + 7].split(" ", 3);
+					
+						Individual tempIndividual = new Individual(sections[1], sectionsName[2], sectionsGender[2], sectionsBirthday[2]);
+					
+						individuals.add(tempIndividual);
+					}
+					
+					if(sections[2].equals("FAM"))
+					{
+						
+						String[] sectionsHusbandsID = lines[i + 1].split(" ", 3);
+						String[] sectionsWifesID = lines[i + 2].split(" ", 3);
+						ArrayList<String> children = new ArrayList<String>();
+						boolean continueLoop = true;
+						int count = 0;
+						while(continueLoop)
+						{
+							String[] isChild = lines[i + 3 + count].split(" ", 3);
+							if(isChild[1].equals("CHIL"))
+							{
+								children.add(isChild[2]);
+								count++;
+							}
+							else
+							{
+								continueLoop = false;
+							}
+						}
+					
+						Family tempFamily = new Family(sections[1], sectionsHusbandsID[2], sectionsWifesID[2], children);
+					
+						families.add(tempFamily);
+					}
 				}
 				else
 				{
@@ -67,6 +122,24 @@ public class main_project
 			
 			valid = "N";
 			c = 0;
+			
+			
+		}
+		
+		System.out.println("************************************");
+		System.out.println("Individuals");
+		
+		for(int j = 0; j < individuals.size(); j++)
+		{
+			System.out.println(individuals.get(j).toTestString());
+		}
+		
+		System.out.println("************************************");
+		System.out.println("Families");
+		
+		for(int j = 0; j < families.size(); j++)
+		{
+			System.out.println(families.get(j).toTestString());
 		}
 	}
 	
@@ -106,6 +179,112 @@ public class main_project
 				}
 			}
 			return false;
+		}
+	}
+	
+	public static class Individual
+	{
+		private String ID;
+		private String name;
+		private String gender;
+		private String birthday;
+		
+		public Individual(String ID, String name, String gender, String birthday)
+		{
+			this.ID = ID;
+			this.name = name;
+			this.gender = gender;
+			this.birthday = birthday;
+		}
+		
+		String getID()
+		{
+			return this.ID;
+		}
+		
+		String getName()
+		{
+			return this.name;
+		}
+		
+		String getGender()
+		{
+			return this.gender;
+		}
+		
+		String getBirthday()
+		{
+			return this.birthday;
+		}
+		
+		String toTestString()
+		{
+			return "ID: " + this.ID + " Name: " + this.name; // + " Gender: " + this.gender + " Birthday: " + this.birthday;
+		}
+	}
+	
+	public static class Family
+	{
+		private String ID;
+		private String husbandID;
+		private String wifeID;
+		private ArrayList<String> children;
+		
+		public Family(String ID, String husbandID, String wifeID, ArrayList<String> children)
+		{
+			this.ID = ID;
+			this.husbandID = husbandID;
+			this.wifeID = wifeID;
+			this.children = children;
+		}
+		
+		String getID()
+		{
+			return this.ID;
+		}
+		
+		String getHusbandID()
+		{
+			return this.husbandID;
+		}
+		
+		String getHusbandName()
+		{
+			for(int j = 0; j < individuals.size(); j++)
+			{
+				if(this.husbandID.equals(individuals.get(j).getID()))
+				{
+					return individuals.get(j).getName();
+				}
+			}
+			return "ERROR";
+		}
+		
+		String getWifeID()
+		{
+			return this.wifeID;
+		}
+		
+		String getWifeName()
+		{
+			for(int j = 0; j < individuals.size(); j++)
+			{
+				if(this.wifeID.equals(individuals.get(j).getID()))
+				{
+					return individuals.get(j).getName();
+				}
+			}
+			return "ERROR";
+		}
+		
+		ArrayList<String> getChildren()
+		{
+			return this.children;
+		}
+		
+		String toTestString()
+		{
+			return "ID: " + this.ID + " Husband ID: " + this.husbandID + " Husband Name: " + getHusbandName() + " Wife ID: " + this.wifeID + " Wife Name: " + getWifeName(); //+ "-" + this.children;
 		}
 	}
 }
